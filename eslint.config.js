ce274f7b1e570c67870c5eb73bbd72ae710e87d7
spiritov <132454615+spiritov@users.noreply.github.com>
2025-04-22 22:41:43 -0700

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import { configs } from '@eslint/js';
+
+export default [
+  configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+    rules: {
+
+    },
+  },
+];
