b1a8da1db4c708a85eec0681136bbb55e718ff35
spiritov <132454615+spiritov@users.noreply.github.com>
2025-04-30 20:25:15 -0700

    update wip paths

--- src/commands/public/ping.js
+++ src/commands/public/ping.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription('latency test'),
   async execute(interaction) {
     const sent = await interaction.deferReply(); //thinking...
-    interaction.editReply(`pong! ${inlineCode(sent.createdTimestamp - interaction.createdTimestamp + 'ms')}`);
+    interaction.reply(`pong! ${inlineCode(sent.createdTimestamp - interaction.createdTimestamp + 'ms')}`);
   },
 };
