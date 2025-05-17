2748e09ba14bd2d32f55d0ff9a71d53a04ebe7bf
spiritov <132454615+spiritov@users.noreply.github.com>
2025-05-17 01:06:50 -0700

    use promises for sqlite3 callback functions

--- src/lib/database.js
+++ src/lib/database.js
@@ -5,10 +5,10 @@ const { createStartJob, createEndJob } = require('./tourney-schedule.js');
 // open connection to sqlite3 db
 function openDB() {
   // reset db for testing
-  db.run(`DROP TABLE IF EXISTS player`);
-  db.run(`DROP TABLE IF EXISTS tournament`);
-  db.run(`DROP TABLE IF EXISTS tournament_player`);
-  db.run(`DROP TABLE IF EXISTS tournament_time`);
+  // db.run(`DROP TABLE IF EXISTS player`);
+  // db.run(`DROP TABLE IF EXISTS tournament`);
+  // db.run(`DROP TABLE IF EXISTS tournament_player`);
+  // db.run(`DROP TABLE IF EXISTS tournament_time`);
 
   db.run(`CREATE TABLE IF NOT EXISTS player (
     id               INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -22,6 +22,7 @@ function openDB() {
 
   db.run(`CREATE TABLE IF NOT EXISTS tournament (
     id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    message_id TEXT NOT NULL,
     class      TEXT NOT NULL,
     plat_gold_map   TEXT NOT NULL,
     silver_map TEXT NOT NULL,
@@ -35,7 +36,8 @@ function openDB() {
   db.run(`CREATE TABLE IF NOT EXISTS tournament_player (
     tournament_id INTEGER NOT NULL,
     player_id     INTEGER NOT NULL,
-    division      TEXT NOT NULL,
+    division      TEXT,
+    signed_up     BOOLEAN NOT NULL,
     created_at    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (tournament_id, player_id),
     FOREIGN KEY (tournament_id) REFERENCES tournament (id),
@@ -51,19 +53,15 @@ function openDB() {
     FOREIGN KEY (player_id) REFERENCES player (id))`);
 }
 
-function getTourneyTimes() {
-  db.each(`SELECT starts_at FROM tournament`, (err, starts_at) => {
-    if (starts_at !== undefined) {
-      console.log(`starts at: ${starts_at}`);
-      createStartJob(starts_at);
-    }
+async function getTourneyTimes() {
+  db.each(`SELECT starts_at FROM tournament`, (err, row) => {
+    console.log(`starts at: ${row.starts_at}`);
+    createStartJob(row.starts_at);
   });
 
-  db.each(`SELECT ends_at FROM tournament`, (err, ends_at) => {
-    if (ends_at !== undefined) {
-      console.log(`ends at: ${ends_at}`);
-      createStartJob(ends_at);
-    }
+  db.each(`SELECT ends_at FROM tournament`, (err, row) => {
+    console.log(`ends at: ${row.ends_at}`);
+    createEndJob(row.ends_at);
   })
 }
 
@@ -72,6 +70,20 @@ function createPlayer(discord_id, display_name) {
   db.run(`INSERT OR IGNORE INTO player (discord_id, display_name)
         VALUES (?, ?)`,
     discord_id, display_name);
+}
+
+async function getPlayerId(discord_id) {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT id FROM player
+    WHERE discord_id = ?`, discord_id, (err, row) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(row.id);
+      }
+    });
+  });
 }
 
 function setDisplayName(discord_id, display_name) {
@@ -89,9 +101,27 @@ function setDivision(discord_id, player_class, division) {
     division, discord_id);
 }
 
+async function getDivision(id, player_class) {
+  console.log(id);
+  console.log(player_class);
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT soldier_division, demo_division FROM player
+    WHERE id = ?`, id, (err, row) => {
+      division = (player_class === 'Soldier') ? row.soldier_division : row.demo_division;
+
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(division);
+      }
+    });
+  });
+}
+
+
 // sets all player divisions from their discord role(s)
 function setDivisionsFromRoles(members) {
-  let numUpdatedRoles = 0;
   members.forEach(member => {
     const soldier_role = member.roles.cache.find((role) => role.name.includes('Soldier'));
     const soldier_div = soldier_role === undefined ? null : `${soldier_role.name.substring(0, soldier_role.name.indexOf(' '))}`;
@@ -121,6 +151,36 @@ function setIds(discord_id, tempus_id, steam_id32) {
     tempus_id, steam_url, discord_id);
 }
 
+function createTournament(message_id, trny) {
+  db.run(`INSERT OR IGNORE INTO tournament (message_id, class, plat_gold_map, silver_map, bronze_map, steel_map, wood_map, starts_at, ends_at)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    message_id, trny.class, trny.plat_gold, trny.silver, trny.bronze, trny.steel, trny.wood, trny.starts_at, trny.ends_at);
+}
+
+async function getLatestTournament() {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT * FROM tournament ORDER BY created_at DESC`, (err, row) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(row);
+      }
+    });
+  });
+}
+
+function createTournamentPlayer(tournament_id, player_id, division, signed_up) {
+  db.run(`INSERT INTO tournament_player (tournament_id, player_id, division, signed_up)
+    VALUES (?, ?, ?, ?)
+    ON CONFLICT DO UPDATE
+    SET signed_up = ?`, tournament_id, player_id, division, signed_up, signed_up);
+}
+
+function removeTournamentPlayer() {
+
+}
+
 function closeDB() {
   db.close();
 }
@@ -129,9 +189,15 @@ module.exports = {
   openDB,
   getTourneyTimes,
   createPlayer,
+  getPlayerId,
   setDisplayName,
   setDivision,
+  getDivision,
   setDivisionsFromRoles,
   setIds,
-  closeDB
+  createTournament,
+  getLatestTournament,
+  createTournamentPlayer,
+  removeTournamentPlayer,
+  closeDB,
 };
