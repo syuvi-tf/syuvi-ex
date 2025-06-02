7cb4d39b3c5bc86a545786e12e73e1fe39955b6b
spiritov <132454615+spiritov@users.noreply.github.com>
2025-06-02 15:17:56 -0700

    tidy main

--- src/main.js
+++ src/main.js
@@ -8,42 +8,27 @@ const { signupsReactionAdd, signupsReactionRemove } = require('./events/signup-r
 const { updateSignupsJob } = require('./lib/schedules.js');
 dotenv.config();
 
-// create a new client instance
-const client = new Client(
-  {
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
-    partials: [Partials.Message, Partials.Reaction]
-  });
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-client.commands = new Collection();
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+function getCommands(client) {
+  const foldersPath = path.join(__dirname, 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
+  client.commands = new Collection();
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+      // set a new item in the Collection with the key as the command name and the value as the exported module
+      if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+      }
     }
   }
 }
 
-// when the client is ready, run this code (only once)
-client.once(Events.ClientReady, readyClient => {
-  console.log(`ready! logged in as ${readyClient.user.tag}`);
-  openDB();
-  // if there is an active tourney, we can run the update job
-  if (getActiveTourney() !== undefined) {
-    updateSignupsJob(client.channels.cache.get(signupsChannelId));
-  }
-});
-
-client.on(Events.InteractionCreate, async interaction => {
+async function runCommand(interaction) {
   if (!interaction.isChatInputCommand()) return;
   const command = interaction.client.commands.get(interaction.commandName);
   if (!command) {
@@ -61,6 +46,29 @@ client.on(Events.InteractionCreate, async interaction => {
       await interaction.reply({ content: 'encountered an error running this command', flags: MessageFlags.Ephemeral });
     }
   }
+}
+
+// create a new client instance
+const client = new Client(
+  {
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+    partials: [Partials.Message, Partials.Reaction]
+  });
+
+getCommands(client);
+
+// when the client is ready, run this code (only once)
+client.once(Events.ClientReady, readyClient => {
+  console.log(`ready! logged in as ${readyClient.user.tag}`);
+  openDB();
+  // if there is an active tourney, we can start the update job
+  if (getActiveTourney() !== undefined) {
+    updateSignupsJob(client.channels.cache.get(signupsChannelId));
+  }
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  runCommand(interaction);
 });
 
 // there should only ever be one signups message in #signups, so checking just the channel id should be fine
