180fde882ee490e29eaf575df6064754e078b8ce
spiritov <132454615+spiritov@users.noreply.github.com>
2025-06-17 01:41:58 -0700

    add echo command

---
+++ src/commands/private/echo.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('duplicate the last message sent in this channel, in another channel')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('#channel to relay the last message to')
+        .setRequired(true)),
+  async execute(interaction) {
+    await interaction.deferReply(); //thinking...
+    const channel = interaction.options.getChannel('channel');
+    try {
+      const echoMessage = (await interaction.channel.messages.fetch({ limit: 2, cache: false })).filter((message) => !message.author.bot).values().next().value;
+      channel.send({ content: echoMessage.content });
+      interaction.editReply(`✅ Message relayed.`);
+    }
+    catch {
+      interaction.editReply(`❌ Ran into an error.. couldn't relay message.`);
+    }
+  },
+};
