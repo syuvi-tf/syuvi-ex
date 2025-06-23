d145513bad6c05551dffadbc2f7cb9beeac187be
spiritov <132454615+spiritov@users.noreply.github.com>
2025-06-23 01:03:32 -0700

    create toptimes command

---
+++ src/commands/public/toptimes.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { getPlayer, createPlayer, getActiveTourney, getRecentTourney } = require('../../lib/database.js');
+const { getPlayerEmbed } = require('../../lib/components.js');
+
+// TODO: create embed
+function getTourneyTopTimesEmbed() {
+  const embed = new EmbedBuilder();
+  return embed;
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('toptimes')
+    .setDescription('display the top 8 times for the current / previous tourney')
+    .addStringOption(option =>
+      option.setName('division')
+        .setDescription('division top times to display'))
+    .addChoices(
+      { name: 'Platinum', value: 'Platinum' },
+      { name: 'Gold', value: 'Gold' },
+      { name: 'Silver', value: 'Silver' },
+      { name: 'Bronze', value: 'Bronze' },
+      { name: 'Steel', value: 'Steel' },
+      { name: 'Wood', value: 'Wood' },),
+  async execute(interaction) {
+    await interaction.deferReply(); //thinking...
+    const trny = getActiveTourney() ?? getRecentTourney();
+    const member = interaction.member;
+    const division_name = interaction.options.getString('division') ?? null;
+    if (!trny) {
+      interaction.editReply(`Couldn't find a tourney to display a leaderboard for..`);
+    }
+    else {
+      if (!division_name) {
+        // no division name, use player's div if they have one
+        const player = getPlayer(member.id) ?? createPlayer(member.id);
+        const player_division = (trny.class === 'Soldier' ? player.soldier_division : player.demo_division);
+        if (player_division) {
+          // embed leaderboard for player's division
+        }
+        else {
+          interaction.editReply(`Couldn't display a leaderboard, as you don't have a ${trny.class} division.`);
+        }
+      }
+      else {
+        // run normally
+        if (trny.class === 'Demo' && division_name === 'Wood') {
+          interaction.editReply('There is no Wood Demo leaderboard..');
+        }
+        else {
+          // embed for selected division
+        }
+      }
+    }
+
+    interaction.editReply({ embeds: [getPlayerEmbed(user, player)] });
+  },
+};
