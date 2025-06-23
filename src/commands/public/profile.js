7bac4edded9eeef542a9ec0be57a6ca5266423d4
spiritov <132454615+spiritov@users.noreply.github.com>
2025-06-23 01:03:50 -0700

    create profile command

---
+++ src/commands/public/profile.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getPlayer, createPlayer } = require('../../lib/database.js');
+const { getPlayerEmbed } = require('../../lib/components.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('profile')
+    .setDescription('view a player\'s profile')
+    .addUserOption(option =>
+      option.setName('player')
+        .setDescription('@mention')),
+  async execute(interaction) {
+    await interaction.deferReply(); //thinking...
+    const user = interaction.options.getUser('player') ?? interaction.user;
+    const player = getPlayer(user.id) ?? createPlayer(user.id);
+
+    interaction.editReply({ embeds: [getPlayerEmbed(user, player)] });
+  },
+};
