edb26d704f5ab2a4b03002d263f8b7f5bce9b041
dresswithpockets <dresswithpockets@pm.me>
2025-06-29 03:10:31 -0700

    clean up createtourneyadd jsdoc plugin to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc"
 
 export default defineConfig([
 	{ files: ["**/*.js"], languageOptions: { globals: globals.node } },
-	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.js"], plugins: { js, jsdoc }, extends: ["js/recommended"], rules: { "jsdoc/no-undefined-types": 1 } },
 ]);
