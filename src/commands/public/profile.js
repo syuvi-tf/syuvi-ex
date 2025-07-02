f896cb4fb52f026be1054ca620c81c348671cb1f
spiritov <132454615+spiritov@users.noreply.github.com>
2025-07-02 13:53:59 -0700

    use member

--- src/commands/public/profile.js
+++ src/commands/public/profile.js
@@ -9,9 +9,9 @@ export default {
     .addUserOption((option) => option.setName("player").setDescription("@user")),
   async execute(interaction) {
     await interaction.deferReply(); //thinking...
-    const member = interaction.options.getMember("player") ?? interaction.user;
+    const member = interaction.options.getMember("player") ?? interaction.member;
     const player = getPlayer(member.id) ?? createPlayer(member.id, member.displayName);
 
-    interaction.editReply({ embeds: [getPlayerEmbed(member, player)] });
+    interaction.editReply({ embeds: [getPlayerEmbed(member.user, player)] });
   },
 };
