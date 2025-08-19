f11875bc14327d0ea58c447e54ecafddf947404b
bailey <spiritov_v@pm.me>
2025-08-18 17:49:57 -0700

    format

--- src/main.js
+++ src/main.js
@@ -1,11 +1,43 @@
-// dev
 import dotenv from 'dotenv';
 import { Client, Collection, GatewayIntentBits, Events, MessageFlags, Partials } from 'discord.js';
-
 import commands from './discord/commands/commands.ts';
 import { test } from './schedule/marathon.ts';
 
 dotenv.config();
+
+// create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMembers,
+  ],
+  partials: [Partials.Message, Partials.Reaction],
+});
+
+updateCommands(client);
+
+// run once when the bot is online
+client.once(Events.ClientReady, (client) => {
+  console.log(`ready! logged in as ${client.user.tag}`);
+
+  // schedule routine to run on bot startup
+
+  // test routine
+  test();
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  executeCommand(interaction);
+});
+
+process.on('beforeExit', () => {
+  // close sqlite database
+  console.log(`attempted to close sqlite database before exiting.`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
 
 async function executeCommand(interaction) {
   if (!interaction.isChatInputCommand()) return;
@@ -43,36 +75,3 @@ function updateCommands(client) {
     client.commands.set(command.data.name, command);
   }
 }
-
-// create a new client instance
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildMembers,
-  ],
-  partials: [Partials.Message, Partials.Reaction],
-});
-
-updateCommands(client);
-
-client.once(Events.ClientReady, (client) => {
-  console.log(`ready! logged in as ${client.user.tag}`);
-
-  // schedule routine to run on bot startup
-
-  // test routine
-  test();
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  executeCommand(interaction);
-});
-
-process.on('beforeExit', () => {
-  // close sqlite database
-  console.log(`attempted to close sqlite database before exiting.`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
