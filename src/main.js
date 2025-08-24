53ebac0872933a56909a5c292c7affe16010fade
bailey <spiritov_v@pm.me>
2025-08-23 18:08:52 -0700

    reduce yelling

--- src/main.js
+++ src/main.js
@@ -46,7 +46,7 @@ async function executeCommand(interaction) {
   if (!interaction.isChatInputCommand()) return;
   const command = interaction.client.commands.get(interaction.commandName);
   if (!command) {
-    console.error(`[ERROR] No command matching ${interaction.commandName} was found.`);
+    console.error(`[error] No command matching ${interaction.commandName} was found.`);
     return;
   }
   try {
@@ -71,7 +71,7 @@ function updateCommands(client) {
   client.commands = new Collection();
   for (const command of commands) {
     if (!command.data || !command.execute) {
-      console.log(`[WARNING] Command is missing a required "data" or "execute" property.`);
+      console.log(`[warning] Command is missing a required "data" or "execute" property.`);
       continue;
     }
 
