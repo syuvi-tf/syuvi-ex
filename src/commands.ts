f36c6df3aa6697cdd8c47aaf65f1aed0bd309523
bailey <spiritov_v@pm.me>
2025-08-31 04:29:39 -0700

    organize

---
+++ src/commands.ts
@@ -0,0 +1,7 @@
+import ping from './discord/commands/test/ping.js';
+import marathon from './discord/commands/marathon.js';
+import testdisplaycomponent from './discord/commands/test/testdisplaycomponent.js';
+
+const commands = [ping, marathon, testdisplaycomponent];
+
+export default commands;
--- src/discord/commands/commands.ts
+++
@@ -1,7 +0,0 @@
-import ping from './test/ping.js';
-import marathon from './marathon.js';
-import testdisplaycomponent from './test/testdisplaycomponent.js';
-
-const commands = [ping, marathon, testdisplaycomponent];
-
-export default commands;
